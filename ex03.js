9e53ca8b75027f3c26cfdd18fdee8f0c3d699c53
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-05-27 16:40:57 -0300

    resolucao do exercicio 03

---
+++ ex03.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")();
+console.clear();
+
+const trechoMusica = prompt("Digite o trecho da música que você mais gosta: ");
+console.log(`Canta comigo: \n${trechoMusica}`);
